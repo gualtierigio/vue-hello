5391073daae2dc882f6d5acfe422d57e71207978
gualtierigio <gualtierigio@gmail.com>
2021-09-14 16:11:58 +0200

    come aggiungo un filtro?

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,11 +2,15 @@ var app = new Vue({
     el: '#app',
     data: {
       message: 'che svolta!',
-      sNFeaver: 'img/travolta.png'
+      sNFeaver: 'img/travolta.png',
+      opacity: 'filter',
     },
     methods: {
         reverseTitle: function () {
             this.message = this.message.split('').reverse('').join('');
         },
-    }
+        filter: function(){
+            this.sNFeaver += this.opacity;
+        }
+    },
   });
