cc92cc502aca9c5db468089361bbdb77baedc4a5
gualtierigio <gualtierigio@gmail.com>
2021-09-14 15:48:44 +0200

    un po di test

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,6 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'che svolta!'
+      message: 'che svolta!',
+      sNFeaver: 'img/travolta.png'
+    },
+    methods: {
+        reverseTitle: function () {
+            this.message = this.message.split('').reverse('').join('');
+        },
     }
-  })
+  });
